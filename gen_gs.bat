3a1f2f2d4986ac05658aeaf91622333656a2712b
dranzerstar <mssh@h>
2022-02-21 21:53:55 +0800

    2102night

--- gen_gs.bat
+++ gen_gs.bat
@@ -1,19 +1,13 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-for /L %%i in (1, 1, 25) do (
+for /L %%i in (1, 1, 7) do (
      set "num=000000%%i"
      set "num=!num:~-2!"
 rem     echo !num!
 
-
 	
-echo	1 = {	modifier = { factor = 0 owner={ has_global_flag = idol_debut_!num! } }
-echo		modifier = { factor = 2 owner={ has_global_flag = idol_grp_id_!num! } }
-echo		owner = { set_global_flag = idol_debut_!num! }
-echo		owner = { set_global_flag = idol_grp_id_!num! }
-echo		change_variable = { which = idol_total value = 1 }
-echo		owner ={ save_global_event_target_as = idol_country_!num! } }
-echo            #
+echo		effectButtonType = { name = "idol_effect_group_!num!_desc" quadTextureSprite = "GFX_idol_effect_bg"
+echo		position = { x = 0 y = 0 }  effect = "idol_effect_group_!num!" text="idol_group_effect_text_!num!" }
                                                                                                       
 )
