dc70041e68a4700206b8ec3e6e56b7b552fb3052
dranzerstar <mssh@h>
2022-02-22 22:56:24 +0800

    2222final

--- gen_gs2.bat
+++ gen_gs2.bat
@@ -1,22 +1,12 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-for /L %%i in (0, 1, 25) do (
+for /L %%i in (1, 1, 25) do (
      set "num=000000%%i"
      set "num=!num:~-2!"
 rem     echo !num!
 
 
-<<<<<<< HEAD
-echo desc = { trigger = { check_variable = {which= idols_select value = !num! }}
-echo	text = Idols_detail_!num! }
-
-	
-
-echo	text = { trigger = { check_variable={ which=live_member_5 value=!num! } } localization_key = idol_live_effect_!num! }
-
-
-echo has_country_flag = live_member_flag_!num!
-
+echo if = { limit={ check_variable = {  which = live_member_5 value = !num! } }  set_country_flag = live_member_flag_!num!  }
 
 )
