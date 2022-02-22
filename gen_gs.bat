30bbe06f5332c9fe83aeb4cd866b37a3e46d30fd
dranzerstar <mssh@h>
2022-02-22 20:44:01 +0800

    0000

--- gen_gs.bat
+++ gen_gs.bat
@@ -1,13 +1,12 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-for /L %%i in (1, 1, 7) do (
+for /L %%i in (1, 1, 25) do (
      set "num=000000%%i"
      set "num=!num:~-2!"
 rem     echo !num!
 
 	
-echo		effectButtonType = { name = "idol_effect_group_!num!_desc" quadTextureSprite = "GFX_idol_effect_bg"
-echo		position = { x = 0 y = 0 }  effect = "idol_effect_group_!num!" text="idol_group_effect_text_!num!" }
+echo	spriteType = { name = "GFX_live_idol_card_!num!" texturefile = "gfx/interface/imas_ui/live_cropped/frame_!num!.dds" masking_texture="gfx/interface/imas_ui/live_frame_mask.dds" }
                                                                                                       
 )
