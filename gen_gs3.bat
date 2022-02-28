6ba8bf9472573581832c7d0c65e6eb4e12764d25
anton <anton>
2022-02-28 17:36:23 +0800

    do event 3000

--- gen_gs3.bat
+++ gen_gs3.bat
@@ -7,5 +7,7 @@ for /L %%i in (1, 1, 25) do (
       set "num=!num:~-2!"
 rem     echo !num!
 
-echo	option={ name=idol_name_with_icon_!num! allow={ hidden_trigger={ NOT={ has_country_flag=live_member_flag_!num! }}}trigger={has_global_flag=idol_debut_!num!} hidden_effect={ set_variable={which=live_member_5 value=!num2! } set_country_flag = live_member_flag_!num!} }
+echo 	if={  event_target:country_hosting_live ={ has_country_flag=live_member_flag_!num! }  add_modifier={ years=5 modifier = idol_live_effect_!num! } }
+
+                                                                                                                                                                                                                                                         
 )
